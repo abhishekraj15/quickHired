17fadfa37f81827f92710eea2afcc5c8276fdeaa
abhishekraj15 <rabhi0766@gmail.com>
2024-09-07 14:30:23 +0530

    first commit

---
+++ src/pages/my-jobs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyJobs = () => {
+  return <div>my-jobs</div>;
+};
+
+export default MyJobs;
---
+++ src/pages/saved-job.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SaveJob = () => {
+  return <div>SavedJob</div>;
+};
+
+export default SaveJob;
