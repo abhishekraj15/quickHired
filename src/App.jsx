dd0b8cbd21b205459868f9d088857730712d1d6a
abhishekraj15 <rabhi0766@gmail.com>
2024-09-21 12:31:35 +0530

    routeIssue

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: "/saved-job",
+        path: "/saved-jobs",
         element: (
           <ProtectedRoute>
             <UnderDevelopmentPage />
